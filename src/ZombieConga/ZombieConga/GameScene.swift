385020ec86925220ee9dd35020a8734637ba148c
altair21 <lovealtair21@gmail.com>
2016-04-05 16:51:42 +0800

    first blood

---
+++ src/ZombieConga/ZombieConga/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  ZombieConga
+//
+//  Created by altair21 on 16/4/5.
+//  Copyright (c) 2016年 altair21. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        self.backgroundColor = SKColor.blackColor()
+    }
+    
+}
