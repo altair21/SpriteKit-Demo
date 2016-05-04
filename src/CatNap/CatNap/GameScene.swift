6cc8f47216d0387e555916369ac54b8aea121152
altair21 <lovealtair21@gmail.com>
2016-05-05 00:25:58 +0800

    remove useless function

--- src/CatNap/CatNap/GameScene.swift
+++ src/CatNap/CatNap/GameScene.swift
@@ -70,26 +70,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         hookNode = self.childNodeWithName("hookBase") as? HookNode
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
